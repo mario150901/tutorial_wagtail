9eb63d53338d7ccda4b0a33ccefd86c4b728ac68
mario150901 <mario150901@gmail.com>
2022-03-11 19:56:25 +0100

    Eliminicación de la clase 'Viaje', que no aportaba nada al modelo y el método paginate lo mismo

--- viajes/models.py
+++ viajes/models.py
@@ -6,27 +6,27 @@ from wagtail.admin.edit_handlers import FieldPanel
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your models here.
-class Viaje(models.Model):
-    nombre = models.CharField('título', max_length=250)
-    #slug = models.SlugField(blank=True)
-    link = models.URLField()
-    imagen = models.URLField()
-    coordenadas = models.CharField("coordenadas", max_length = 250, blank=True)
+# class Viaje(models.Model):
+#     nombre = models.CharField('título', max_length=250)
+#     #slug = models.SlugField(blank=True)
+#     link = models.URLField()
+#     imagen = models.URLField()
+#     coordenadas = models.CharField("coordenadas", max_length = 250, blank=True)
 
-    panels = [
-        FieldPanel('nombre'),
-        FieldPanel('link'),
-        FieldPanel('imagen'),
-        FieldPanel('coordenadas')
+#     panels = [
+#         FieldPanel('nombre'),
+#         FieldPanel('link'),
+#         FieldPanel('imagen'),
+#         FieldPanel('coordenadas')
 
-    ]
+#     ]
 
-    def __str__(self):
-        return f'{self.nombre}'
+#     def __str__(self):
+#         return f'{self.nombre}'
     
-    class Meta:
-        verbose_name = 'Viaje'
-        verbose_name_plural = 'Viajes'
+#     class Meta:
+#         verbose_name = 'Viaje'
+#         verbose_name_plural = 'Viajes'
 
 class ViajesPage(Page):
     texto = RichTextField(blank=True)
@@ -36,14 +36,14 @@ class ViajesPage(Page):
         FieldPanel('coordenadas', classname="full")
     ]
 
-    def paginate(self, request, viajes, *args):
-        page = request.GET.get('page')
+    # def paginate(self, request, viajes, *args):
+    #     page = request.GET.get('page')
         
-        paginator = Paginator(viajes, 10)
-        try:
-            pages = paginator.page(page)
-        except PageNotAnInteger:
-            pages = paginator.page(1)
-        except EmptyPage:
-            pages = paginator.page(paginator.num_pages)
-        return pages
+    #     paginator = Paginator(viajes, 10)
+    #     try:
+    #         pages = paginator.page(page)
+    #     except PageNotAnInteger:
+    #         pages = paginator.page(1)
+    #     except EmptyPage:
+    #         pages = paginator.page(paginator.num_pages)
+    #     return pages
